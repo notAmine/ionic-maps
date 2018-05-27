0a378268f24d7ab1873599f4462f08a2d15a5754
Mohamed Amine <medaminebenslama@gmail.com>
2018-05-27 23:11:32 +0100

    add cinemas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var google: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapRef: ElementRef;
+  map: any;
+  constructor(public navCtrl: NavController) {}
 
+  ionViewDidLoad(){
+    this.showMap();
+  }
+
+  showMap(){
+    //location - lat long
+    const location = new google.maps.LatLng(36.831072, 10.147438);
+
+    //Map options
+    const options = {
+      center: location,
+      zoom:10
+    };
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarker(location, this.map);
+  }
+
+  addMarker(position, map){
+    return new google.maps.Marker({
+      position, map
+    });
   }
 
 }
